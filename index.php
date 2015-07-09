2233ef27be6abece17b396c897c07bcbfc66642d
tikenn <tikennprogrammer@gmail.com>
2015-07-08 22:50:52 -0500

    Updated index.php for new versions

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
       <li>HTML: &lt;html&gt;, &lt;head&gt; &lt;title&gt;, &lt;body&gt;, &lt;h1&gt;, &lt;ul&gt;, &lt;li&gt;, &lt;form&gt;, &lt;input&gt;, &lt;select&gt; &lt;option&gt;, &lt;table&gt;, &lt;tr&gt;, &lt;th&gt;, &lt;td&gt;, &lt;br&gt;</li>
       <li>PHP: variables, for, while, increment (++), decrement (--), mysqli, MySQL prepared statements, interspersing HTML and PHP</li>
       <li>SQL: CREATE, INSERT, SELECT</li>
+      <li>HTTP: GET, POST</li>
     </ul>
     <h2><a href="0.1/">0.1</a></h2>
     <ul>
@@ -20,7 +21,24 @@
     </ul>
     <h2><a href="0.2/">0.2</a></h2>
     <ul>
-      <li>PHP: php-login</li>
+      <li>HTML: form attributes (required, pattern, email)</li>
+      <li>PHP: php-login-minimal, str_replace, basic file handling (fopen, fgets, fclose)</li>
+      <li>Design: header file, basic error checking</li>
+    </ul>
+    <h2><a href="0.3/">0.3</a></h2>
+    <ul>
+      <li>PHP: php-login-advanced</li>
+      <li>Design: footer file</li>
+    </ul>
+    <h2><a href="0.4/">0.4</a></h2>
+    <ul>
+      <li>HTML: &lt;nav&gt;, &lt;div&gt;, &lt;span&gt;</li>
+      <li>PHP: stripos, more file handling (file_exists, fopen [append mode], fread, filesize, fwrite)</li>
+      <li>Design: menu file, Bootstrap (navbar, forms, buttons, and tables)</li>
+    </ul>
+    <h2><a href="0.5/">0.5</a></h2>
+    <ul>
+      <li>Design: Modals</li>
     </ul>
   </body>
 </html>
